062c0d92267e3311d72df19bedd6823716aeed7f
Sargent <json0309@163.com>
2022-07-04 14:16:46 +0800

    first commit

---
+++ src/main/java/com/coco/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.coco.git;
+
+/**
+ * @Description:
+ * @Author: Sargent
+ * @Data: 2022/7/4 14:12
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
